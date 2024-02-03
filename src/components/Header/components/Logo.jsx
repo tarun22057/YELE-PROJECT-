4a2c20c474612fd44d9de7104be48a5d9cf7d893
Arnav Sharma <sharmaarnav626@gmail.com>
2024-02-03 18:51:22 +0530

    setup

---
+++ src/components/Header/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "";
+
+const Logo = () => {
+  return (
+    <div className="logo-img">
+      <img src={Logo} alt="logo"></img>
+    </div>
+  );
+};
+
+export default Logo;
